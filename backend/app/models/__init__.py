aa798857a62d5b0f98c4e07703ba039e394e2a34
fernygo64 <fernygo64@hotmail.com>
2026-09-02 11:12:20 -0500

    Eliminar

--- backend/app/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from app.models.models import (  # noqa: F401
-    Empresa, CuentaContable, Proveedor, CentroCosto, ReglaContable,
-    ImportacionHistorico, HistorialContable, Auditoria,
-    SistemaContable, OrigenDecision,
-    CargaDocumentosDian, Factura, FuenteExtraccion, EstadoFactura,
-    Movimiento, TipoMovimiento, PlantillaExportacion, Exportacion, EstadoExportacion,
-    ConfiguracionComprobanteSiigo, ConsecutivoSiigo, ParametrizacionCuentaSiigo, ExportacionFactura,
-    PucCuenta,
-)
